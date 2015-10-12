2a9a74b1082d81272c91f447eea9056f916b7460
Christian Carlson <christiancarlson@Amelias-MacBook-Pro.local>
2015-10-11 17:01:55 -0700

    added music, and some styling

--- game.js
+++ game.js
@@ -1,45 +1,71 @@
 $(document).ready(function() {
 
-	    function RandomWord() {
-        var requestStr = "http://api.wordnik.com:80/v4/words.json/randomWords?hasDictionaryDef=true&minCorpusCount=0&minLength=5&maxLength=15&limit=1&api_key=a2928d7b39887c8f9340f0f28c303d6e15b37e8871ea72361";
+    var array = undefined;
+
+    var drawHangman = function() {
+        array.forEach(function(letter) {
+            $('.word').append('<h2 class="letter">' + letter + '</h2>');
+
+        });
+    };
+
+        //function to get a random word from Wordnik API
+	var randomWord = function(minLength, maxLength)  {
+        var requestStr = "http://api.wordnik.com:80/v4/words.json/randomWords?minCorpusCount=10000&minDictionaryCount=20&excludePartOfSpeech=proper-noun,proper-noun-plural,proper-noun-posessive,suffix,family-name,idiom,affix&hasDictionaryDef=true&includePartOfSpeech=noun,verb,adjective,definite-article,conjunction&limit=26&minLength="+minLength+"&maxLength="+maxLength+"&api_key=a2928d7b39887c8f9340f0f28c303d6e15b37e8871ea72361";
 
         $.ajax(requestStr,
         	{
         	method: 'GET',
             success: function(data) {
-            	// var array = data.split('');
-                console.log(data[0].word);
-                // console.log(array.length);
-      			
+            	array = (data[0].word).toLowerCase().split('');
+                drawHangman();	
             }
         }
         );
     }
 
-   RandomWord();
+    //show rules when hovering mouse over How to Play?
+    $('h2').mouseenter(function() {
+        $('#des').show();
+    }).mouseleave(function() {
+        $('#des').hide();
+    });
 
-    // word.forEach.(function(letter) {
-    //     $('<h3>' + letter + '</h3>').appendTo('div');
-    // });
-     
+    //function when play button is clicked, shows diff buttons
+    $('#play').on('click', function() {
+        $('.intro').hide();
+        $('.dif_buttons').show();
+    });
 
- 
+    //set randomWord parameters when diff buttons are clicked
+
+    $('#easy').on('click', function() {
+        randomWord(3,5);
+        $('.diff').hide();
+        
+    });
+
+    $('#med').on('click', function() {
+        randomWord(6,9);
+        $('.diff').hide();
+        
+    });
+
+    $('#hard').on('click', function() {
+        randomWord(10,15);
+        $('.diff').hide(); 
+    });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    //button to toggle music on and off
+    $("#music").on("click", function(){
+            var x = document.querySelector("#gbu");
+            if (x.paused === false){
+                x.pause();
+                $("#vol").removeClass("glyphicon glyphicon-volume-up").addClass("glyphicon glyphicon-volume-off");
+            } else {
+                x.play();
+                $("#vol").removeClass("glyphicon glyphicon-volume-off").addClass("glyphicon glyphicon-volume-up");
+            }
+        });
 });
