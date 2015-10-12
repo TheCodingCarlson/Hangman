3b155e21eda305b6d9a87e0a1ab30efcf1b651ab
Christian Carlson <christiancarlson@Amelias-MacBook-Pro.local>
2015-10-12 14:34:22 -0700

    added more styling and functionality

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+
     //set global variables
     var array = undefined;
     var turnCount = 6;
@@ -12,19 +13,56 @@ $(document).ready(function() {
         });
     };
 
-    //function to check if letter chosen by player is in the word/show letter if guess is correct/
+    //function to check if letter chosen by player is in the word/show letter if guess is wrong/decrement turn count
     var checkLetter = function() {
         $('.input').on('click', function() {
-            turnCount--;
             $('.turns_left').html('Turns Left: ' + turnCount);
             console.log(turnCount);
             var letter = $(this).attr('id');
+            var boo = false;
+
             array.forEach(function(x) {
-                if(letter == x) {
+                if(letter === x && !$('.'+letter).data('displayed')) {
                     $('.'+letter).show();
+                    $('.'+letter).data('displayed', true);
+                    boo = true;
+                } else if(letter === x) {
+                    alert('This letter has already been chosen!');
+                    boo = true;
                 }
             });
+
+            if(boo === false) {
+                turnCount--;
+                updateTurnCount();
+                alert('Sorry, ' + letter + ' is not in this word. Try again!');
+            }
+            determineOutcome();
         });
+    }
+
+    //Updates turn count to HTML
+    var updateTurnCount = function() {
+        $('.turns_left').html('Turns Left: ' + turnCount);
+    }
+
+    var determineOutcome = function() {
+        var correctLetterCount = 0;
+        var divs = $('.let');
+
+        if(turnCount === 0) {
+            alert('You Lose');
+        } else {
+            for(var i=0; i < divs.length; i++) {
+                if($(divs[i]).css('display') === 'none') { //telling me that the letter is wrong
+                    correctLetterCount++;
+                }
+            }
+            if(0 === correctLetterCount) {
+                alert('You win!');
+
+            }
+        }
     }
 
     //function to get a random word from Wordnik API
@@ -39,15 +77,15 @@ $(document).ready(function() {
                 console.log(array);
                 displayWord();	
                 checkLetter();
-                $('.turns_left').html('Turns Left: ' + turnCount);
+                updateTurnCount();
             }
         }
         );
     }
 
-
-
-
+    //hide input board/hangman
+    $('.board').hide();
+    $('.hangman').hide();
 
     //show rules when hovering mouse over How to Play?
     $('h2').mouseenter(function() {
@@ -56,29 +94,33 @@ $(document).ready(function() {
         $('#des').hide();
     });
 
-    //function when play button is clicked, shows difficulty buttons
+    //function when play button is clicked, shows difficulty buttons/hides intro div
     $('#play').on('click', function() {
         $('.intro').hide();
         $('.dif_buttons').show();
     });
 
-    //set randomWord parameters when difficulty buttons are clicked
+    //set randomWord parameters when difficulty buttons are clicked/hide the diff buttons/show input board
 
     $('#easy').on('click', function() {
         randomWord(3,5);
         $('.diff').hide();
-        
+        $('.board').show();
+        $('.hangman').show(); 
     });
 
     $('#med').on('click', function() {
         randomWord(6,9);
         $('.diff').hide();
-        
+        $('.board').show();
+        $('.hangman').show();  
     });
 
     $('#hard').on('click', function() {
         randomWord(10,15);
-        $('.diff').hide(); 
+        $('.diff').hide();
+        $('.board').show(); 
+        $('.hangman').show();
     });
 
 
